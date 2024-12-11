0c147b154a7f973a7b56684f1df893f86a67567b
aliceaupaysdesdata <169068020+aliceaupaysdesdata@users.noreply.github.com>
2024-12-11 15:46:54 +0100

    maj dossier streamlit

--- streamlit/streamlite.py
+++ streamlit/streamlite.py
@@ -1,1 +1,48 @@
+# La commande du Terminal pour lancer le site : 
+# streamlit run .\streamlit\streamlite.py
+# OU
+# streamlit run https://github.com/aliceaupaysdesdata/Movie-recommendation-project/raw/refs/heads/main/streamlit/streamlite.py
+
+
+# Importer les bibliothèques nécessaires
+
 import streamlit as st
+from streamlit_authenticator import Authenticate
+import pandas as pd
+
+
+# Configuration de la page : 
+
+st.set_page_config(page_title="Le 23ème Écran", layout="wide")
+
+
+# Style CSS pour personnaliser le design : 
+
+# st.markdown("""
+#    <style>
+#        /* Fond général */
+#            """)
+
+
+# Page d'accueil : 
+
+st.title("Le 23ème Écran vous fait découvrir le cinéma")
+
+
+# Bar de menu : 
+
+# trouver le code pour
+
+
+# Bloc d'affichage des films : 
+
+# df = le résultat de la recherche de similarité
+# st.table(df)
+
+
+# Authentification simple : 
+
+# Faire la quête streamlit avancée
+
+
+#
